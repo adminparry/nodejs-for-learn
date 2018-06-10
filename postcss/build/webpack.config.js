9c40891d9ce855861681aec48eb6f12c8e0bfed0
pparry <1170015014@qq.com>
2018-06-11 01:06:20 +0800

    postcss

---
+++ postcss/build/webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var rootPath = p => path.resolve(__dirname,'../',p);
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin({
+			filename:"stylesheets/[name].css",
+
+		});
+
+const html = new HtmlWebpackPlugin({
+            template: rootPath('src/index.html')
+        })
+module.exports = {
+	mode:'development',
+	watch:true,
+	entry:rootPath('src/main.js'),
+	output:{
+		filename:'bundle.js',
+		path:rootPath('dist')
+	},
+	module:{
+		rules:[
+			{
+				test:/\.js$/,
+				use:'babel-loader'
+			},
+			{
+				test:/\.(css|sass|less|styl)$/,
+				use:extractCSS.extract(['css-loader','postcss-loader'])
+			}
+		]
+	},
+	plugins:[
+		extractCSS,
+		html
+	]
+}
