ea252f8a73e4c7a4676e68b424c83ccfd6b41b2d
pparry <1170015014@qq.com>
2018-06-11 11:20:30 +0800

    no message

--- postcss/build/webpack.config.js
+++ postcss/build/webpack.config.js
@@ -1,39 +1,40 @@
 var path = require('path');
-var rootPath = p => path.resolve(__dirname,'../',p);
+var rootPath = p => path.resolve(__dirname, '../', p);
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractCSS = new ExtractTextPlugin({
-			filename:"stylesheets/[name].css",
+    filename: "stylesheets/[name].css",
 
-		});
+});
 
 const html = new HtmlWebpackPlugin({
-            template: rootPath('src/index.html')
-        })
+    template: rootPath('src/index.html')
+})
 module.exports = {
-	mode:'development',
-	watch:true,
-	entry:rootPath('src/main.js'),
-	output:{
-		filename:'bundle.js',
-		path:rootPath('dist')
-	},
-	module:{
-		rules:[
-			{
-				test:/\.js$/,
-				use:'babel-loader'
-			},
-			{
-				test:/\.(css|sass|less|styl)$/,
-				use:extractCSS.extract(['css-loader','postcss-loader'])
-			}
-		]
-	},
-	plugins:[
-		extractCSS,
-		html
-	]
+    mode: 'development',
+    watch: true,
+    entry: rootPath('src/main.js'),
+    output: {
+        filename: 'bundle.js',
+        path: rootPath('dist')
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(css|sass|less|styl)$/,
+                use: extractCSS.extract([
+                    'css-loader', 'postcss-loader',
+                ])
+            }
+        ]
+    },
+    plugins: [
+        extractCSS,
+        html
+    ]
 }
